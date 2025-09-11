4de05fc0544f085553946ed3f8eeabbe4739d5f4
Wahid Patel <patel.wahid2005@gmail.com>
2025-09-11 18:34:27 +0530

    removed gpt version

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -22,7 +22,7 @@ function About() {
     >
         <div className='bg-zinc-800/50 p-7 rounded-2xl md:p-12 max-w-6xl mx-auto reveal-up'>
             <p className='text-zinc-300 mb-4 md:8 md:text-xl md:max-w-[90ch]'>
-                Hi, I’m Wahid Patel — an aspiring Software Engineer and Frontend Developer.
+                Hi, I’m Wahid Patel  an aspiring Software Engineer and Frontend Developer.
                 I’m currently pursuing a B.E. in Information Technology at M.H. Saboo Siddik College of Engineering (Batch 2027). With hands-on experience in React.js, JavaScript, Python, C++, and SQL, I enjoy building scalable web applications and solving real-world problems through technology.
                 <br /> <br />
                 Beyond academics, I serve as the Vice Chairperson of the ACM Student Chapter, where I lead and collaborate on technical events, hackathons, and student initiatives.
